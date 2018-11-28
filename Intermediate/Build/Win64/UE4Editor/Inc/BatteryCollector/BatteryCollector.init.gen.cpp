d6038223aeb7f187d5aa2d46be444b5205432b25
Max Sazhaev <maxsazhaev@gmail.com>
2018-11-27 21:42:05 -0500

    created new C++ class for pickups

--- Intermediate/Build/Win64/UE4Editor/Inc/BatteryCollector/BatteryCollector.init.gen.cpp
+++ Intermediate/Build/Win64/UE4Editor/Inc/BatteryCollector/BatteryCollector.init.gen.cpp
@@ -19,8 +19,8 @@ void EmptyLinkFunctionForGeneratedCodeBatteryCollector_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/BatteryCollector",
 				PKG_CompiledIn | 0x00000000,
-				0xCD278B20,
-				0x3D95CCFB,
+				0xED4A7DC5,
+				0x7F197C69,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
 			};
